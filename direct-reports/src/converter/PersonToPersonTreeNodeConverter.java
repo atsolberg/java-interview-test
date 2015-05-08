0a477f337694c085c177b8e3ae262cf090694b3c
Aaron Solberg <atsolberg@gmail.com>
2015-05-08 09:16:44 -0500

    initial commit

---
+++ direct-reports/src/converter/PersonToPersonTreeNodeConverter.java
@@ -0,0 +1,23 @@
+package converter;
+
+import model.Person;
+import model.PersonTreeNode;
+
+/**
+ * Instructions: 
+ *  Complete {@link #generateTree(Iterable)} method such that the unit test will run and pass
+ *  Create any additional methods as needed
+ */
+public class PersonToPersonTreeNodeConverter {
+
+	/**
+     * Given an iterable of {@link Person}s, will produce the root person tree node (the CEO)
+     * and each person's list of direct reports is correct all the way down the tree.
+     */
+	public PersonTreeNode generateTree(Iterable<Person> allEmployees) {
+		
+		// TODO ENTER CODE HERE
+    	return null;
+    }
+	
+}
